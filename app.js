b1837cac5a7966d9600cc303c321405af08925a0
Andy Almeida <73726361+andyalpa@users.noreply.github.com>
2024-09-19 00:08:18 +0000

    Currency Converter Done

---
+++ app.js
@@ -0,0 +1,31 @@
+// One euro is:
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+
+const fromEuroToDollar = (valueInEuro) => {
+    const exchangeRateUSD = oneEuroIs["USD"];
+    let valueInDollar = valueInEuro * exchangeRateUSD;
+    return valueInDollar;
+}
+const fromDollarToYen = (valueInDollar) => {
+    return (valueInDollar / 1.07) * 156.5 ;
+  };
+
+const fromYenToPound = (valueInYen) => {
+    return (valueInYen / 156.5) * 0.87;
+}
+
+const euroAmount = 100; // Assume 100 Euros
+const dollarAmount = fromEuroToDollar(euroAmount);
+const yenAmount = fromDollarToYen(dollarAmount);
+const poundAmount = fromYenToPound(10000);
+
+console.log(`${euroAmount} Euros is approximately ${dollarAmount} US Dollars.`);
+console.log(`${dollarAmount} US Dollars is approximately ${yenAmount} Japanese Yen.`);
+console.log(`${10000} Japanese Yen is approximately ${poundAmount} British Pounds.`);
+
+
+module.exports = { fromDollarToYen, fromEuroToDollar, fromYenToPound };
